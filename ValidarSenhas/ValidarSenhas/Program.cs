2a2cc2e7c9c864ad13689ad719cdcbdcd30f9c72
wDavidAraujo <113621516+wDavidAraujo@users.noreply.github.com>
2023-04-04 22:45:16 -0300

    Nesse projeto em C# eu fiz um validador de senhas
    O validador testa as seguintes caracteristicas
    
    Nove ou mais caracteres
    Ao menos 1 dígito
    Ao menos 1 letra minúscula
    Ao menos 1 letra maiúscula
    Ao menos 1 caractere especial
    Considere como especial os seguintes caracteres: !@#$%^&*()-+
    Não possuir caracteres repetidos dentro do conjunto

---
+++ ValidarSenhas/ValidarSenhas/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Linq;
+
+namespace DesafioSenha {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Olá, como você se chama?");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Certo {nome}, qual a sanha que você quer cadastrar?");
+            string senha = Console.ReadLine();
+
+            int contagemespecial = senha.Split(new char[] { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+' }).Length - 1;
+
+            if (senha.Length >= 9) {
+                if (!senha.Any(char.IsDigit)) {
+                    Console.WriteLine("ERRO! Sua senha não tem 1 ou mais números!");
+                    System.Environment.Exit(0);
+                }
+                else if (contagemespecial == 0) {
+                    Console.WriteLine("ERRO! Sua senha não tem 1 ou mais caracteris especiais!");
+                    System.Environment.Exit(0);
+                }
+                else if (!senha.Any(char.IsUpper)) {
+                    Console.WriteLine("ERRO! Sua senha não tem 1 ou mais letras maiusculas!");
+                    System.Environment.Exit(0);
+                }
+                else if (!senha.Any(char.IsLower)) {
+                    Console.WriteLine("ERRO! Sua senha não tem 1 ou mais letra minuscula!");
+                    System.Environment.Exit(0);
+                }
+                else if (senha.ToCharArray().Distinct().ToArray().Length != senha.Length) {
+                    Console.WriteLine("ERRO! Sua senha tem uma caractere repetido!");
+                    System.Environment.Exit(0);
+                }
+
+                Console.WriteLine($"Sucesso {nome}! Sua senha foi cadastrada.");
+            }
+            else if (senha.Length < 9) {
+                Console.WriteLine("ERRO! Sua senha não tem 9 ou mais caracteris!");
+            }
+        }
+    }
+}

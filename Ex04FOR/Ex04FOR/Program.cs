c10e50980d9324961eb1b66f21f2f01e2bf31922
wDavidAraujo <113621516+wDavidAraujo@users.noreply.github.com>
2023-05-07 00:56:57 -0300

    Add files via upload

---
+++ Ex04FOR/Ex04FOR/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex04FOR {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("quantos pares fazer fazer as divisões?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine("digite um par de números para dividirmos:");
+                float n1 = int.Parse(Console.ReadLine());
+                float n2 = int.Parse(Console.ReadLine());
+
+                if (n2 == 0) {
+                    Console.WriteLine("divisão impossível");
+                }
+                else {
+                    float resultado = n1 / n2;
+                    Console.WriteLine($"sua divisão teve resultado igual á:{resultado}");
+                }
+            }
+        }
+    }
+}

0af2583eb7a82ebcbcdb06d79daa67b13c0358d7
wDavidAraujo <113621516+wDavidAraujo@users.noreply.github.com>
2023-05-07 00:54:05 -0300

    Add files via upload

---
+++ EstruturaDeRepeticao/EstruturaDeRepeticao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+namespace EstruturaDeRepeticao {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digita um número para eu dize qual é a raiz dele! ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0.0) {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("f3", CultureInfo.InvariantCulture));
+                Console.Write("Digita outra parada ai ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Você acabou de digitar um número negativo!");
+        }
+    }
+}
---
+++ Ex01WHILE/Ex01WHILE/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex01WHILE {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("digite sua senha (somente números) ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002) {
+                Console.Write("senha inválida, tente novamente! ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("acesso permitido!");
+        }
+    }
+}
